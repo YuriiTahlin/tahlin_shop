33a48f7c9134f96e431781bcf0edf88dc458dd74
Yurii Tahlin <yurii.taglin@gmail.com>
2025-02-23 13:03:22 +0200

    v2.0.2: Implemented an array of React components for dynamic rendering

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import Card from "./components/Card";
 import Header from "./components/Header";
 import Drawer from "./components/Drawer";
 
+const arr = [
+    { title: "iPhone 13", price: 20999, imageUrl: "/img/sneakers/1.png" },
+    { title: "iPhone 14", price: 24999, imageUrl: "/img/sneakers/2.png"},
+    { title: "iPhone 15", price: 29999, imageUrl: "/img/sneakers/3.png"},
+    { title: "iPhone 16", price: 39999, imageUrl: "/img/sneakers/4.png"}
+];
+
 function App() {
     return (
         <div className="wrapper">
@@ -11,57 +18,19 @@ function App() {
                 <div className="search-block">
                     <h1>Всі моделі iPhone</h1>
                     <div className="search">
-                        <img src="/img/search.svg" alt="Search" width="11" height="11" />
-                        <input placeholder="Пошук" />
+                        <img src="/img/search.svg" alt="Search" width="11" height="11"/>
+                        <input placeholder="Пошук"/>
                     </div>
                 </div>
 
                 <div className="sneakers">
-                    <Card />
-                    <div className="card">
-                        <div className="favorite">
-                            <img src="/img/react-unliked.svg" alt="Unliked" />
-                        </div>
-                        <img width="150" height="170" src="/img/sneakers/2.png" alt="Product" />
-                        <h5>iPhone 14</h5>
-                        <div className="price">
-                            <span>Ціна:</span>
-                            <b>24999 грн</b>
-                            <div className="button">
-                                <img className="addButton" src="/img/icon.svg" alt="Icon" width="25" height="25"/>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="card">
-                        <div className="favorite">
-                            <img src="/img/react-unliked.svg" alt="Unliked" />
-                        </div>
-                        <img width="150" height="170" src="/img/sneakers/3.png" alt="Product" />
-                        <h5>iPhone 15</h5>
-                        <div className="price">
-                            <span>Ціна:</span>
-                            <b>33999 грн</b>
-                            <div className="button">
-                                <img className="addButton" src="/img/icon.svg" alt="Icon" width="25" height="25"/>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="card">
-                        <div className="favorite">
-                            <img src="/img/react-unliked.svg" alt="Unliked" />
-                        </div>
-                        <img width="150" height="170" src="/img/sneakers/4.png" alt="Product" />
-                        <h5>iPhone 16</h5>
-                        <div className="price">
-                            <span>Ціна:</span>
-                            <b>42999 грн</b>
-                            <div className="button">
-                                <img className="addButton" src="/img/icon.svg" alt="Icon" width="25" height="25"/>
-                            </div>
-                        </div>
-                    </div>
+                    {arr.map((obj) => (
+                    <Card
+                        title={obj.title}
+                        price={obj.price}
+                        imageUrl={obj.imageUrl}
+                    />
+                        ))}
                 </div>
             </div>
         </div>
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,16 +1,16 @@
 import App from "../App";
 
-function Card() {
+function Card(props) {
     return (
         <div className="card">
             <div className="favorite">
                 <img src="/img/react-unliked.svg" alt="Unliked"/>
             </div>
-            <img width="150" height="170" src="/img/sneakers/1.png" alt="Product"/>
-            <h5>iPhone 13</h5>
+            <img width="150" height="170" src={props.imageUrl} alt="Product"/>
+            <h5>{props.title}</h5>
             <div className="price">
                 <span>Ціна:</span>
-                <b>20999 грн</b>
+                <b>{props.price}</b>
                 <div className="button">
                     <img className="addButton" src="/img/icon.svg" alt="Icon" width="25" height="25"/>
                 </div>
